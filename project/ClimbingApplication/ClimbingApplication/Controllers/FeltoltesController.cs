462aca30159d1f3de3f422e0487b403c87c4e80c
meszarosmarton1007 <meszarosmarton1007@gmail.com>
2025-05-26 15:08:21 +0200

    kép feltöltés backend kód hozzáadása, mégn nem jó

---
+++ project/ClimbingApplication/ClimbingApplication/Controllers/FeltoltesController.cs
@@ -0,0 +1,66 @@
+﻿using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Storage.V1;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClimbingApplication.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FeltoltesController : ControllerBase
+    {
+        private static readonly string bucketNev = "climbingapplication.appspot.com";
+        private static bool firebaseInistalized = false;
+
+        public FeltoltesController()
+        {
+            if (!firebaseInistalized)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "secrets", "firebase-adminsdk.json");
+                Console.WriteLine("Firebase path: " + path);
+
+                FirebaseApp.Create(new AppOptions
+                {
+                    Credential = GoogleCredential.FromFile(path),
+                    ProjectId = "iotcloud2025"
+                });
+                firebaseInistalized = true;
+            }
+        }
+        public class ImageDataRequest
+        {
+            public string ImageData { get; set; }
+        }
+
+            [HttpPost]
+            public async Task<IActionResult> POST([FromBody] ImageDataRequest request)
+            {
+            try
+            {
+
+                if (string.IsNullOrEmpty(request.ImageData))
+                {
+                    return BadRequest("Nincs képadat.");
+                }
+                var base64 = request.ImageData.Split(',')[1];
+                var imageBytes = Convert.FromBase64String(base64);
+
+                var imageName = $"climbingapplication/images/{Guid.NewGuid()}.png";
+
+                var storage = StorageClient.Create();
+                using (var stream = new MemoryStream(imageBytes))
+                {
+                    await storage.UploadObjectAsync(bucketNev, imageName, "image/png", stream);
+                }
+
+                var publicUrl = $"https://storage.googleapis.com/{bucketNev}/{imageName}";
+                return Ok(new { url = publicUrl });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Hiba: {ex.Message}");
+            }
+            }
+        }
+    }
+
--- project/ClimbingApplication/ClimbingApplication/Program.cs
+++ project/ClimbingApplication/ClimbingApplication/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<EFContextcs>();
 
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
